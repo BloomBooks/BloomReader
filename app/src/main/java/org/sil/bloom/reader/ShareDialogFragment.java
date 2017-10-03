6310298aecfb9701191f0bef982d86ba7742a12d
Rick Conrad <richardjconrad@gmail.com>
2017-10-03 16:22:37 +0100

    Cleanup code for SharingManager and ShareDialogFragment

--- app/src/main/java/org/sil/bloom/reader/ShareDialogFragment.java
+++ app/src/main/java/org/sil/bloom/reader/ShareDialogFragment.java
@@ -6,17 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 /**
  * Created by rick on 10/2/17.
+ * Much of it borrowed from the ShareDialogFragment for Scripture/ReadingAppBuilder
  */
 
 public class ShareDialogFragment extends DialogFragment {
-//    private static final String LOG_TAG_ANALYTICS = "AB-Analytics";
-//    private static final String SHARE_TYPE_LINK = "share-link";
-//    private static final String SHARE_TYPE_FILE = "share-file";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,74 +22,24 @@ public class ShareDialogFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.dialog_share, container, false);
 
-
-        // Graphic
-        ImageView img = (ImageView) view.findViewById(R.id.imgGraphic);
-        img.setAdjustViewBounds(true);
-
-        // Calculate hues
-//        int originalImgColor = Color.rgb(45, 78, 107);
-//        int originalImgHue   = (int)GraphicsUtils.colorHue(originalImgColor);
-
-//        int color = getStyleColor(CommonStyleName.UI_ACTION_BAR, PropertyName.COLOR_TOP);
-//        int hue   = (int)GraphicsUtils.colorHue(color);
-
-//        if (hue > 0) {
-//            // Use coloured image, modifying hue for current color scheme
-//            Drawable d = ResourcesCompat.getDrawable(getResources(), R.drawable.app_sharing, null);
-//            img.setImageDrawable(d);
-//
-//            int rotation = hue - originalImgHue;
-//            ColorFilter filter = ColorFilterGenerator.adjustColor(0, 0, 0, rotation);
-//            img.setColorFilter(filter);
-//        }
-//        else {
-            // Use grayscale image
-//            Drawable d = ResourcesCompat.getDrawable(getResources(), R.drawable.app_sharing_grey, null);
-//            img.setImageDrawable(d);
-////        }
-//
-//        img.requestLayout();
-
-
-        // Share Link
         Button btnShareLink = (Button) view.findViewById(R.id.btnShareLink);
-//        if (getAppDefinition().getAppConfig().hasFeature(CommonFeatureName.SHARE_APP_LINK)) {
+        btnShareLink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new SharingManager(getActivity()).shareLinkToAppOnGooglePlay();
+                dismiss();
+            }
+        });
 
-            btnShareLink.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    new SharingManager(getActivity()).shareLinkToAppOnGooglePlay();
-//                    SharingManager mgr = new SharingManager(getActivity(), getAppDefinition());
-//                    trackShare(SHARE_TYPE_LINK);
-//                    mgr.shareLinkToAppOnGooglePlay();
-                    dismiss();
-                }
-            });
-//        }
-//        else {
-//            btnShareLink.setVisibility(View.GONE);
-//        }
-
-        // Share APK
         Button btnShareApk = (Button) view.findViewById(R.id.btnShareApkFile);
-//        if (getAppDefinition().getAppConfig().hasFeature(CommonFeatureName.SHARE_APK_FILE)) {
+        btnShareApk.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new SharingManager(getActivity()).shareApkFile();
+                dismiss();
+            }
+        });
 
-            btnShareApk.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    SharingManager mgr = new SharingManager(getActivity());
-                    mgr.shareApkFile();
-                    dismiss();
-                }
-            });
-//        }
-//        else {
-//            btnShareApk.setVisibility(View.GONE);
-//        }
-
-        // Cancel button
         Button btnCancel = (Button) view.findViewById(R.id.btnCancel);
         btnCancel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -101,15 +47,8 @@ public class ShareDialogFragment extends DialogFragment {
             }
         });
         btnCancel.setTextColor(getResources().getColor(R.color.accent_material_light));
-        btnCancel.setText(getString(R.string.cancel));
 
-        // Allow tap outside dialog to close it
         getDialog().setCanceledOnTouchOutside(isCanceledOnTouchOutside());
-
-        // Colors
-//        String bgColorStr = getAppDefinition().getAppConfig().getStylePropertyColorValue(CommonStyleName.UI_DIALOG, BACKGROUND_COLOR);
-//        int bgColor = GraphicsUtils.parseColor(bgColorStr, Color.WHITE);
-//        view.setBackgroundColor(bgColor);
 
         return view;
     }
@@ -121,18 +60,4 @@ public class ShareDialogFragment extends DialogFragment {
     protected void onCloseDialog() {
         dismiss();
     }
-
-//    private void trackShare(String shareType) {
-//        AppDefinition app = getAppDefinition();
-//        if (app.getAppConfig().getAnalytics().isEnabled()) {
-//            String appName = app.getAppName();
-//            String appVersion = app.getVersionName();
-//            Log.i(LOG_TAG_ANALYTICS, String.format("SharingManager::TrackShare: type=%s, name=%s, version=%s", shareType, appName, appVersion));
-//            AnalyticsEventShareApp event = new AnalyticsEventShareApp();
-//            event.withAttribute(AnalyticsEventShareApp.KEY_SHARE_APP_NAME, appName)
-//                    .withAttribute(AnalyticsEventShareApp.KEY_SHARE_APP_VERSION, appVersion)
-//                    .withAttribute(AnalyticsEventShareApp.KEY_SHARE_TYPE, shareType);
-//            getAppApplication().getAnalyticsManager().trackShareApp(event);
-//        }
-//    }
 }
--- app/src/main/java/org/sil/bloom/reader/SharingManager.java
+++ app/src/main/java/org/sil/bloom/reader/SharingManager.java
@@ -2,14 +2,10 @@ package org.sil.bloom.reader;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.content.FileProvider;
-import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
@@ -20,46 +16,27 @@ import java.io.IOException;
 
 /**
  * Created by rick on 10/2/17.
+ * Much of it borrowed from the SharingManager for Scripture/ReadingAppBuilder
  */
 
 public class SharingManager {
-    private static final String LOG_TAG_SHARING = "BloomReader-Sharing";
 
     private Context mContext;
-//    private AppDefinition mApp;
 
     public SharingManager(Context context) {
         mContext = context;
-//        mApp = app;
     }
 
     public void shareBook(Book book){
         File bookFile = new File(book.path);
         String dialogTitle = mContext.getString(R.string.share) + " " + book.name;
         Uri uri = FileProvider.getUriForFile(mContext, "org.sil.bloom.reader.fileprovider", bookFile);
-//        shareFile(bookFile, "*/*", dialogTitle);
         shareFile(uri, "*/*", dialogTitle);
     }
 
     public void shareApkFile() {
         try {
-//        File apk = new File(mContext.getApplicationInfo().publicSourceDir);
-            String uri = mContext.getApplicationInfo().publicSourceDir;
-//            // Copy APK and rename it to the original APK filename
-//            // (The Android system often renames it to base.apk)
-
-            File srcApk = new File(uri);
-            File destApk = new File(sharedApkPath());
-
-            if (destApk.exists() && destApk.isFile()) {
-                // Delete destination file if it exists already
-                destApk.delete();
-                destApk = new File(sharedApkPath());
-            }
-            FileUtils.copyFile(srcApk, destApk);
-
-//            if (FileUtils.fileExists(destFilename)) {
-
+            File apkFile = copyApkToExternalStorageForSharing();
             // vnd.android.package-archive is the correct type but
             // bluetooth is not listed as an option for this on
             // older versions of android
@@ -67,69 +44,44 @@ public class SharingManager {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
                 type = "application/vnd.android.package-archive";
 
-//            shareFile(destApk, type, mContext.getString(R.string.share_app_via));
-            shareFile(Uri.fromFile(destApk), type, mContext.getString(R.string.share_app_via));
+            shareFile(Uri.fromFile(apkFile), type, mContext.getString(R.string.share_app_via));
         }
 
         catch(IOException e){
             Toast failToast = Toast.makeText(mContext, "Failed to share apk file.", Toast.LENGTH_LONG);
             failToast.show();
         }
-        //======================================================================
-
-//        String appUri = mContext.getApplicationInfo().publicSourceDir;
-//
-////        if (StringUtils.isNotBlank(appUri)) {
-//            // Copy APK and rename it to the original APK filename
-//            // (The Android system often renames it to base.apk)
-//            String destFolder = Environment.getExternalStorageDirectory().getAbsolutePath();
-//            String destFilename = destFolder + "/" + "BloomReader.apk";
-//
-//            Log.i(LOG_TAG_SHARING, "From: " + appUri);
-//            Log.i(LOG_TAG_SHARING, "To:   " + destFilename);
-//
-//            File srcApk  = new File(appUri);
-//            File destApk = new File(destFilename);
-//
-//            if (destApk.exists() && destApk.isFile()) {
-//                // Delete destination file if it exists already
-//                destApk.delete();
-//                destApk = new File(destFilename);
-//            }
-//
-//            FileUtils.copyFile(srcApk, destApk);
-//
-//            if (FileUtils.fileExists(destFilename)) {
-//                // Share...
-//                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-//                intent.putExtra(Intent.EXTRA_SUBJECT, mApp.getAppName());
-//
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                    // This is the correct MIME type for APK files
-//                    // Seems to work ok for Android 4.4 and above
-//                    intent.setType("application/vnd.android.package-archive");
-//                }
-//                else {
-//                    // If MIME type is set correctly for Gingerbread phones,
-//                    // Bluetooth is not listed as an option, so use */* instead
-//                    intent.setType("*/*");
-//                }
-//
-//                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(destApk));
-//
-//                Intent chooser = Intent.createChooser(intent, getString(CommonStringId.SHARE_APK_FILE_VIA)); // "Share App via"
-//                mContext.startActivity(chooser);
-//            }
-//            else {
-//                Log.e(LOG_TAG_SHARING, "File not found: " + destFilename);
-//            }
-//        }
     }
 
-//    private void shareFile(File file, String fileType, String dialogTitle){
-    private void shareFile(Uri uri, String fileType, String dialogTitle){
-        //Uri uri = FileProvider.getUriForFile(mContext, "org.sil.bloom.reader.fileprovider", file);
+    public void shareLinkToAppOnGooglePlay() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
 
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Bloom Reader");
+
+            String sAux = "\n" + mContext.getString(R.string.recommend_app) + "\n\n" +
+                    "https://play.google.com/store/apps/details?id=" + mContext.getPackageName() + " \n\n";
+            intent.putExtra(Intent.EXTRA_TEXT, sAux);
+
+            Intent chooser = Intent.createChooser(intent, mContext.getString(R.string.share_app_via));
+            mContext.startActivity(chooser);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // We have to stage the apk file in public storage for sharing.
+    // This gets called now and then to delete the file there if it's more than a day old.
+    public static void fileCleanup(){
+        File sharedApkFile = new File(sharedApkPath());
+        long yesterday = System.currentTimeMillis() - (1000 * 60 * 60 * 24);
+        if(sharedApkFile.exists() && sharedApkFile.lastModified() < yesterday)
+            sharedApkFile.delete();
+    }
+
+    private void shareFile(Uri uri, String fileType, String dialogTitle){
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -139,11 +91,16 @@ public class SharingManager {
         mContext.startActivity(Intent.createChooser(shareIntent, dialogTitle));
     }
 
-    public static void fileCleanup(){
-        File sharedApkFile = new File(sharedApkPath());
-        long yesterday = System.currentTimeMillis() - (1000 * 60 * 60 * 24);
-        if(sharedApkFile.exists() && sharedApkFile.lastModified() < yesterday)
-            sharedApkFile.delete();
+    private File copyApkToExternalStorageForSharing() throws IOException{
+        String apkPath = mContext.getApplicationInfo().publicSourceDir;
+        File srcApk = new File(apkPath);
+        File destApk = new File(sharedApkPath());
+        if (destApk.exists() && destApk.isFile()) {
+            destApk.delete();
+            destApk = new File(sharedApkPath());
+        }
+        FileUtils.copyFile(srcApk, destApk);
+        return destApk;
     }
 
     private static String sharedApkPath(){
@@ -151,32 +108,4 @@ public class SharingManager {
         sharedApkPath += File.separator + "BloomReader.apk";
         return sharedApkPath;
     }
-
-    /**
-     * Shares link to app on Google Play
-     */
-    public void shareLinkToAppOnGooglePlay() {
-        try {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Bloom Reader");
-
-            // "I can recommend this app:"
-            String sAux = "\n" + mContext.getString(R.string.recommend_app) + "\n\n" +
-                    "https://play.google.com/store/apps/details?id=" + mContext.getPackageName() + " \n\n";
-            intent.putExtra(Intent.EXTRA_TEXT, sAux);
-
-            Intent chooser = Intent.createChooser(intent, mContext.getString(R.string.share_app_via)); // "Share Link via"
-            mContext.startActivity(chooser);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-//    private String getString(final String id) {
-//        return StringManager.INSTANCE.getString(id);
-//    }
-
 }

2715ec985c33ad8edaa17c0ed52d834ff2252d5b
andrew-polk <andrew_polk@sil.org>
2020-02-25 14:29:42 -0700

    Fix another NullReferenceException found in Play Console

--- app/src/test/java/org/sil/bloom/reader/models/BookOrShelfTest.java
+++ app/src/test/java/org/sil/bloom/reader/models/BookOrShelfTest.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class BookOrShelfTest {
@@ -15,13 +16,40 @@ public class BookOrShelfTest {
     public void alphabeticalComparator_sortsAlphabetically() {
 
         List<BookOrShelf> books = Arrays.asList(
-            new BookOrShelf("/dummypath/c"),
-            new BookOrShelf("/dummypath/a"),
-            new BookOrShelf("/dummypath/B")
+                new BookOrShelf("/dummypath/c"),
+                new BookOrShelf("/dummypath/a"),
+                new BookOrShelf("/dummypath/B")
         );
         Collections.sort(books, BookOrShelf.AlphabeticalComparator);
         assertThat(books.get(0).name, is("a"));
         assertThat(books.get(1).name, is("B"));
         assertThat(books.get(2).name, is("c"));
+    }
+    @Test
+    public void alphabeticalComparator_edgeCases_sortsAlphabeticallyWithNullsLast() {
+
+        List<BookOrShelf> books = Arrays.asList(
+                new BookOrShelf("z", null),
+                new BookOrShelf("z", "a"),
+                null,
+                new BookOrShelf("y", "b"),
+                new BookOrShelf("y", "a"),
+                new BookOrShelf(null, "b"),
+                new BookOrShelf(null, "a")
+        );
+        Collections.sort(books, BookOrShelf.AlphabeticalComparator);
+        assertThat(books.get(0).name, is("a"));
+        assertThat(books.get(0).path, is("y"));
+        assertThat(books.get(1).name, is("a"));
+        assertThat(books.get(1).path, is("z"));
+        assertThat(books.get(2).name, is("a"));
+        assertThat(books.get(2).path, nullValue());
+        assertThat(books.get(3).name, is("b"));
+        assertThat(books.get(3).path, is("y"));
+        assertThat(books.get(4).name, is("b"));
+        assertThat(books.get(4).path, nullValue());
+        assertThat(books.get(5).name, nullValue());
+        assertThat(books.get(5).path, is("z"));
+        assertThat(books.get(6), nullValue());
     }
 }

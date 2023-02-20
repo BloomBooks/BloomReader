a023aea060b470ee80d407ad31b699c04ad84a9c
Andrew Polk <andrew_polk@sil.org>
2023-02-20 14:15:25 -0700

    Fix some crashes seen in the Play console

--- app/src/main/java/org/sil/bloom/reader/BookReadyView.java
+++ app/src/main/java/org/sil/bloom/reader/BookReadyView.java
@@ -2,14 +2,11 @@ package org.sil.bloom.reader;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
-import java.io.File;
 
 // This class implements the notification at the bottom of BloomLibraryActivity (or MainActivity)
 // saying that a book (or books) have been downloaded and offering to show them.
@@ -21,12 +18,10 @@ public class BookReadyView extends ConstraintLayout {
     // string that signifies that multiple books have been downloaded and we just offer to go to
     // the main activity to view them.
     private String mBookPath;
-    private Context mParent;
-    private DownloadsView mDownloadsView;
+    private final DownloadsView mDownloadsView;
 
     public BookReadyView(@NonNull Context context, DownloadsView downloadsView) {
         super(context);
-        mParent = context;
         mDownloadsView = downloadsView;
         initializeViews(context);
     }
@@ -36,14 +31,10 @@ public class BookReadyView extends ConstraintLayout {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.book_ready_layout, this);
 
-        mReadNowButton = (Button)this
+        mReadNowButton = this
                 .findViewById(R.id.book_ready_read_now);
-        mReadNowButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View view) {
-                    mDownloadsView.viewBooks(mBookPath);
-            }
-        });
-        mMessage = (TextView) this.findViewById(R.id.book_ready_message);
+        mReadNowButton.setOnClickListener(view -> mDownloadsView.viewBooks(mBookPath));
+        mMessage = this.findViewById(R.id.book_ready_message);
         updateMessage(); // possibly we got the book path before we finished inflating.
     }
 
@@ -56,9 +47,9 @@ public class BookReadyView extends ConstraintLayout {
         if (this.mBookPath == null || mMessage == null) {
             return;
         }
-        String label = getContext().getString(mBookPath == "" ? R.string.downloads_are_complete : R.string.download_is_complete);
+        String label = getContext().getString("".equals(mBookPath) ? R.string.downloads_are_complete : R.string.download_is_complete);
         mMessage.setText(label);
-        String buttonLabel = getContext().getString(mBookPath == "" ? R.string.view_books : R.string.read_now);
+        String buttonLabel = getContext().getString("".equals(mBookPath) ? R.string.view_books : R.string.read_now);
         mReadNowButton.setText(buttonLabel);
     }
 }

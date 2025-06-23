94d203089b1dc2906960761b93a6ae374da9dac5
Nathan Striebel <njstriebel@gmail.com>
2025-06-23 10:33:25 -0400

    added current Bloom Reader version to the top of the about popup (BL-14406)

--- app/src/main/java/org/sil/bloom/reader/MainActivity.java
+++ app/src/main/java/org/sil/bloom/reader/MainActivity.java
@@ -550,16 +550,11 @@ public class MainActivity extends BaseActivity
         // Insert the build version and date into the appropriate control.
         // We have to find it indirectly through the navView's header or it won't be found
         // this early in the view construction.
-        TextView versionDate = navigationView.getHeaderView(0).findViewById(R.id.versionDate);
-        try {
-            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-            Date buildDate = new Date(BuildConfig.TIMESTAMP);
-            String date = DateFormat.format("dd MMM yyyy", buildDate).toString();
-            versionDate.setText(getVersionAndDateText(versionName, date));
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+        String versionDateText = getVersionAndDateText();
+        if(versionDateText != null) {
+            TextView versionDateUIElement = navigationView.getHeaderView(0).findViewById(R.id.versionDate);
+            versionDateUIElement.setText(versionDateText);
         }
-
 
         // Cleans up old-style thumbnails - could be removed someday after it's run on most devices with old-style thumbnails
         BookCollection.cleanUpOldThumbs(this);
@@ -630,8 +625,19 @@ public class MainActivity extends BaseActivity
         configureActionBar(toggle);
     }
 
-    private String getVersionAndDateText(String versionName, String date) {
+    private String getVersionAndDateText() {
         // Not bothering trying to internationalize this for now...
+
+        String versionName = "", date = "";
+
+        try {
+            versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            Date buildDate = new Date(BuildConfig.TIMESTAMP);
+            date = DateFormat.format("dd MMM yyyy", buildDate).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+
         return versionName + ", " + date;
     }
 
@@ -1142,7 +1148,12 @@ public class MainActivity extends BaseActivity
         } else if (id == R.id.nav_test_location_analytics) {
                 showLocationAnalyticsData();
         } else if (id == R.id.about_reader) {
-                DisplaySimpleResource(getString(R.string.about_bloom_reader), R.raw.about_reader);
+                String aboutTitle = getString(R.string.about_bloom_reader);
+                String versionAndDate = getVersionAndDateText();
+                if( versionAndDate != null ) {
+                    aboutTitle = aboutTitle + "\n" + versionAndDate;
+                }
+                DisplaySimpleResource(aboutTitle, R.raw.about_reader);
         } else if (id == R.id.about_bloom) {
                 DisplaySimpleResource(getString(R.string.about_bloom), R.raw.about_bloom);
         } else if (id == R.id.about_sil) {
